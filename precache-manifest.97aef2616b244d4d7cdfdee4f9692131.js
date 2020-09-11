d62178dc8861d8e77234e20598399f796a591bb3
saurabh <saurabh@gmail>
2020-09-11 11:45:10 +0530

    Updates

---
+++ precache-manifest.97aef2616b244d4d7cdfdee4f9692131.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10d98aca6e05a22f820bbffa3fc43be3",
+    "url": "/SaurabhRai19/Newsappapisite.git/index.html"
+  },
+  {
+    "revision": "ce116b019e38d19c51d0",
+    "url": "/SaurabhRai19/Newsappapisite.git/static/css/main.11bd4ca9.chunk.css"
+  },
+  {
+    "revision": "945ec92d4c5f3d56e54e",
+    "url": "/SaurabhRai19/Newsappapisite.git/static/js/2.a9949c24.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/SaurabhRai19/Newsappapisite.git/static/js/2.a9949c24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce116b019e38d19c51d0",
+    "url": "/SaurabhRai19/Newsappapisite.git/static/js/main.45d562d9.chunk.js"
+  },
+  {
+    "revision": "6793c5fd9f026cde3741",
+    "url": "/SaurabhRai19/Newsappapisite.git/static/js/runtime-main.6a7b544d.js"
+  }
+]);
